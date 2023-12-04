52674a03e22bcadb810b16867255c59b8394db5f
Andrey Bondar <andrey_bondar@outlook.com>
2023-12-04 20:37:07 +0200

    make build

---
+++ index.a268f0fb.js
@@ -0,0 +1,2 @@
+document.querySelectorAll(".field-text").forEach(function(e){var t=e.getAttribute("name"),l=e.getAttribute("id"),r=document.createElement("label");r.className="field-label",r.textContent=t,r.setAttribute("for",l),e.after(r),e.placeholder=t.slice(0,1).toUpperCase()+t.slice(1)});
+//# sourceMappingURL=index.a268f0fb.js.map
